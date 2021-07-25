f9fba418e5fb1fda6ccc89c299a38b1c77d8a122
Michael R <tekmedias@gmail.com>
2021-07-25 14:09:44 +0200

    :zap: feat(assets): add build nodeJS and install webpack-encore bundle and configs assets for scss and js

--- www/src/Controller/Admin/DashboardController.php
+++ www/src/Controller/Admin/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,18 +16,33 @@ class DashboardController extends AbstractDashboardController
      */
     public function index(): Response
     {
-        return parent::index();
+        // return parent::index();
+        return 'ok';
+
+        // you can also render some template to display a proper Dashboard
+        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
+        //${APP_DIR_NAME}
     }
 
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Kodo');
+            ->setTitle('KodoTalks')
+            ->setFaviconPath('favicon.svg');
     }
 
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::section('Configuration');
+        yield MenuItem::linktoDashboard('Item for Administrator', 'fa fa-tools')->setPermission('ROLE_ADMIN');
+
+        /*
+        if ($this->isGranted('ROLE_EDITOR')) {
+            yield MenuItem::linkToCrud('Blog Posts', null, BlogPost::class);
+        }
+        */
+        // yield MenuItem::linkToCrud('Blog Posts', null, BlogPost::class)->setPermission('ROLE_USER');
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
     }
 }
